00c4f36e54ec150dda540c718b5f9c4997678e21
antalagota92 <antalagota92@gmail.com>
2017-01-08 18:26:09 +0200

    Additional buttons for each Friend.

--- app/src/main/java/com/datm/agota/findyourfriends/FindLocation.java
+++ app/src/main/java/com/datm/agota/findyourfriends/FindLocation.java
@@ -4,25 +4,48 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import static com.datm.agota.findyourfriends.R.layout.activity_find_location;
+
 public class FindLocation extends AppCompatActivity {
+
+    final Friend[] myFriends = FriendContainer.getFriends();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_find_location);
+        setContentView(activity_find_location);
 
-        final TextView locationField = (TextView) findViewById(R.id.locationField);
         Button refreshLocationButton = (Button) findViewById(R.id.refreshLocationButton);
+
+        setButtonsForMyFriends();
 
         refreshLocationButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent i = new Intent(FindLocation.this, MapsActivity.class);
                 i.putExtra("name", "Bogyo");
-                startActivity(i);            }
+                startActivity(i);
+            }
         });
 
     }
+
+    protected void setButtonsForMyFriends() {
+        LinearLayout layout = (LinearLayout)findViewById(R.id.activity_first_app);
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        for (int i = 0; i < myFriends.length; i++) {
+            Button btn = new Button(this);
+            btn.setText(myFriends[i].getName());
+            btn.setHeight(150);
+            btn.setWidth(150);
+            layout.addView(btn);
+        }
+    }
+
 }
